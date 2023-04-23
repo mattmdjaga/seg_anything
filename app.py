df3310e6f1c526b2ea8e898508ffc589d8de9a85
mattmdjaga <mattmdjaga@gmail.com>
2023-04-23 15:07:23 +0100

    App now loads image, , shows default points and gets the masks for it

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import streamlit as st
 import numpy as np
 import torch
-from PIL import Image
+from PIL import Image, ImageDraw
 import requests
 from transformers import SamModel, SamProcessor
 from io import BytesIO
 import base64
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Load model and processor
+model = SamModel.from_pretrained("facebook/sam-vit-base").to(device)
+processor = SamProcessor.from_pretrained("facebook/sam-vit-base")
+
+# Load image
 
 st.set_page_config(layout="wide", page_title="Segment Anything")
 st.write("## Get a mask from a point in an image")
@@ -23,13 +32,36 @@ def convert_image(img):
     return byte_im
 
 def fix_image(upload):
+    points = [[[115*2, 150*2], [140*2,200*2]]]
     image = Image.open(upload)
+    image = image.resize((image.width // 2, image.height // 2))
+    draw = ImageDraw.Draw(image)
+    for point in points[0]:
+        draw.ellipse((point[0] - 5, point[1] - 5, point[0] + 5, point[1] + 5), fill="red")
+    #draw.ellipse((points[0][0][0] - 5, points[0][0][1] - 5, points[0][0][0] + 5, points[0][0][1] + 5), fill="red")
     col1.write("Original Image :camera:")
     col1.image(image)
 
-    fixed = image.resize((image.width // 4, image.height // 4))
-    col2.write("Fixed Image :wrench:")
-    col2.image(fixed)
+    inputs = processor(image, input_points=points, return_tensors="pt").to(device)
+
+    # Forward pass
+    outputs = model(**inputs)
+
+    # Postprocess outputs
+
+
+    masks = processor.image_processor.post_process_masks(
+        outputs.pred_masks.cpu(), inputs["original_sizes"].cpu(), inputs["reshaped_input_sizes"].cpu()
+    )
+    scores = outputs.iou_scores
+
+    mask = masks[0].squeeze(0).numpy().transpose(1, 2, 0)
+    col2.write("Mask :mask:")
+    for i in range(mask.shape[2]):
+        #mask[:,:,i] = mask[:,:,i] * scores[0][i].item()
+        fixed = Image.fromarray((mask[:,:,i] * 255).astype(np.uint8))
+        col2.write(f"Mask {i} with score {scores[0][0][i].item():.2f}")
+        col2.image(fixed)
     st.sidebar.markdown("\n")
     st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
 
@@ -42,36 +74,4 @@ if my_upload is not None:
 else:
     fix_image("car.png")
 
-'''
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-# Load model and processor
-model = SamModel.from_pretrained("facebook/sam-vit-base").to(device)
-processor = SamProcessor.from_pretrained("facebook/sam-vit-base")
-
-# Load image
-url = "https://huggingface.co/ybelkada/segment-anything/resolve/main/assets/car.png"
-image = Image.open(requests.get(url, stream=True).raw)
-
-# Preprocess image by making it 4x smaller
-image = image.resize((image.width // 4, image.height // 4))
-
-points = [[[115, 150]]]
-
-inputs = processor(image, input_points=points, return_tensors="pt").to(device)
-
-# Forward pass
-outputs = model(**inputs)
-
-# Postprocess outputs
-
-
-masks = processor.image_processor.post_process_masks(
-    outputs.pred_masks.cpu(), inputs["original_sizes"].cpu(), inputs["reshaped_input_sizes"].cpu()
-)
-scores = outputs.iou_scores
-
-mask = masks[0].squeeze(0).numpy().transpose(1, 2, 0)
-
-'''
